39d4103ea95f6f6017da44b4f67a2da3980324b0
ghostheep2020 <ghostheep1998@163.com>
2020-06-27 15:29:15 +0800

    Initial commit

---
+++ Data/readYaml.py
@@ -0,0 +1,7 @@
+import yaml
+
+with open("./data2.yml","r",encoding="utf-8") as f:
+    # 使用yaml加载数据
+    data = yaml.safe_load(f)  # 返回数据
+    print("返回字典数据：",data)
+    print("返回数据类型：",type(data.get("value2")))
